cf0eb083dc34401664a1b1fbd9776fa6719d7d78
wangwangbox <wwbox@duck.com>
2026-06-21 02:10:09 +0800

    ! UDP performance

--- src/udp_proxy.c
+++ src/udp_proxy.c
@@ -15,6 +15,7 @@
 #include <haproxy/buf-t.h>
 #include <haproxy/chunk.h>
 #include <haproxy/fd.h>
+#include <haproxy/global.h>
 #include <haproxy/list.h>
 #include <haproxy/listener.h>
 #include <haproxy/obj_type.h>
@@ -25,22 +26,95 @@
 #include <haproxy/tools.h>
 
 #define UDP_PROXY_SESS_TIMEOUT_MS 60000
+#define UDP_PROXY_HASH_BITS 12
+#define UDP_PROXY_HASH_SIZE (1U << UDP_PROXY_HASH_BITS)
+#define UDP_PROXY_HASH_MASK (UDP_PROXY_HASH_SIZE - 1)
+#define UDP_PROXY_GC_BUCKETS_PER_RUN 4
 
 struct udp_proxy_session {
-	struct list list;
+	struct list by_hash;
 	struct listener *listener;
 	struct server *srv;
 	struct sockaddr_storage client;
+	unsigned int hash;
 	int fd;
 	int expire;
 };
 
-static struct list udp_proxy_sessions = LIST_HEAD_INIT(udp_proxy_sessions);
-__decl_spinlock(udp_proxy_lock);
+struct udp_proxy_shard {
+	struct list buckets[UDP_PROXY_HASH_SIZE];
+	unsigned int gc_bucket;
+	int initialized;
+};
+
+static struct udp_proxy_shard udp_proxy_shards[MAX_THREADS];
 
 static int udp_proxy_sendto(int fd, const void *buf, size_t len,
                             const struct sockaddr_storage *addr);
 static void udp_proxy_delete_session(struct udp_proxy_session *sess);
+
+static void udp_proxy_shard_init(struct udp_proxy_shard *shard)
+{
+	unsigned int i;
+
+	if (likely(shard->initialized))
+		return;
+
+	for (i = 0; i < UDP_PROXY_HASH_SIZE; i++)
+		LIST_INIT(&shard->buckets[i]);
+
+	shard->gc_bucket = 0;
+	shard->initialized = 1;
+}
+
+static unsigned int udp_hash_mix(unsigned int hash, unsigned int value)
+{
+	hash ^= value;
+	hash *= 16777619U;
+	return hash;
+}
+
+static unsigned int udp_hash_bytes(unsigned int hash, const void *data, size_t len)
+{
+	const unsigned char *ptr = data;
+
+	while (len--)
+		hash = udp_hash_mix(hash, *ptr++);
+
+	return hash;
+}
+
+static unsigned int udp_proxy_hash_key(const struct listener *l,
+                                       const struct sockaddr_storage *addr)
+{
+	unsigned int hash = 2166136261U;
+
+	hash = udp_hash_bytes(hash, &l, sizeof(l));
+	hash = udp_hash_mix(hash, addr->ss_family);
+
+	switch (addr->ss_family) {
+	case AF_INET: {
+		const struct sockaddr_in *sin = (const struct sockaddr_in *)addr;
+
+		hash = udp_hash_bytes(hash, &sin->sin_addr, sizeof(sin->sin_addr));
+		hash = udp_hash_bytes(hash, &sin->sin_port, sizeof(sin->sin_port));
+		break;
+	}
+	case AF_INET6: {
+		const struct sockaddr_in6 *sin6 = (const struct sockaddr_in6 *)addr;
+
+		hash = udp_hash_bytes(hash, &sin6->sin6_addr, sizeof(sin6->sin6_addr));
+		hash = udp_hash_bytes(hash, &sin6->sin6_port, sizeof(sin6->sin6_port));
+		hash = udp_hash_bytes(hash, &sin6->sin6_scope_id, sizeof(sin6->sin6_scope_id));
+		break;
+	}
+	default:
+		hash = udp_hash_bytes(hash, addr, get_addr_len(addr));
+		break;
+	}
+
+	return hash;
+}
 
 static int udp_addr_match(const struct sockaddr_storage *a,
                           const struct sockaddr_storage *b)
@@ -64,23 +138,37 @@ static struct server *udp_proxy_pick_server(struct proxy *px)
 	return fallback;
 }
 
-static void udp_proxy_prune_locked(void)
+static void udp_proxy_prune_bucket(struct udp_proxy_shard *shard, unsigned int bucket)
 {
 	struct udp_proxy_session *sess, *back;
 
-	list_for_each_entry_safe(sess, back, &udp_proxy_sessions, list) {
+	list_for_each_entry_safe(sess, back, &shard->buckets[bucket], by_hash) {
 		if (tick_is_expired(sess->expire, now_ms))
 			udp_proxy_delete_session(sess);
 	}
 }
 
-static struct udp_proxy_session *udp_proxy_find_client_locked(struct listener *l,
-                                                             const struct sockaddr_storage *client)
+static void udp_proxy_gc(struct udp_proxy_shard *shard)
+{
+	unsigned int i;
+
+	for (i = 0; i < UDP_PROXY_GC_BUCKETS_PER_RUN; i++) {
+		udp_proxy_prune_bucket(shard, shard->gc_bucket);
+		shard->gc_bucket = (shard->gc_bucket + 1) & UDP_PROXY_HASH_MASK;
+	}
+}
+
+static struct udp_proxy_session *udp_proxy_find_client(struct udp_proxy_shard *shard,
+                                                       struct listener *l,
+                                                       const struct sockaddr_storage *client,
+                                                       unsigned int hash)
 {
 	struct udp_proxy_session *sess;
 
-	list_for_each_entry(sess, &udp_proxy_sessions, list) {
-		if (sess->listener == l && udp_addr_match(&sess->client, client))
+	list_for_each_entry(sess, &shard->buckets[hash & UDP_PROXY_HASH_MASK], by_hash) {
+		if (sess->hash == hash &&
+		    sess->listener == l &&
+		    udp_addr_match(&sess->client, client))
 			return sess;
 	}
 
@@ -125,6 +213,13 @@ static int udp_proxy_connect_session(struct udp_proxy_session *sess)
 	if (fd == -1)
 		return 0;
 
+	if (global.tune.backend_rcvbuf)
+		setsockopt(fd, SOL_SOCKET, SO_RCVBUF, &global.tune.backend_rcvbuf,
+		           sizeof(global.tune.backend_rcvbuf));
+	if (global.tune.backend_sndbuf)
+		setsockopt(fd, SOL_SOCKET, SO_SNDBUF, &global.tune.backend_sndbuf,
+		           sizeof(global.tune.backend_sndbuf));
+
 	addr = sess->srv->addr;
 	set_host_port(&addr, sess->srv->svc_port);
 	if (connect(fd, (struct sockaddr *)&addr, get_addr_len(&addr)) == -1) {
@@ -145,7 +240,7 @@ static int udp_proxy_connect_session(struct udp_proxy_session *sess)
 
 static void udp_proxy_delete_session(struct udp_proxy_session *sess)
 {
-	LIST_DEL_INIT(&sess->list);
+	LIST_DEL_INIT(&sess->by_hash);
 	if (sess->fd >= 0) {
 		fd_delete(sess->fd);
 		close(sess->fd);
@@ -157,17 +252,23 @@ static struct udp_proxy_session *udp_proxy_get_session(struct listener *l,
                                                        const struct sockaddr_storage *client,
                                                        struct server *srv)
 {
+	struct udp_proxy_shard *shard = &udp_proxy_shards[tid];
 	struct udp_proxy_session *sess;
+	unsigned int hash;
 
-	HA_SPIN_LOCK(OTHER_LOCK, &udp_proxy_lock);
-	udp_proxy_prune_locked();
-	sess = udp_proxy_find_client_locked(l, client);
+	udp_proxy_shard_init(shard);
+	udp_proxy_gc(shard);
+
+	hash = udp_proxy_hash_key(l, client);
+	udp_proxy_prune_bucket(shard, hash & UDP_PROXY_HASH_MASK);
+	sess = udp_proxy_find_client(shard, l, client, hash);
 	if (!sess) {
 		sess = calloc(1, sizeof(*sess));
 		if (sess) {
-			LIST_INIT(&sess->list);
+			LIST_INIT(&sess->by_hash);
 			sess->listener = l;
 			memcpy(&sess->client, client, sizeof(*client));
+			sess->hash = hash;
 			sess->fd = -1;
 			sess->srv = srv;
 			if (!udp_proxy_connect_session(sess)) {
@@ -175,14 +276,14 @@ static struct udp_proxy_session *udp_proxy_get_session(struct listener *l,
 				sess = NULL;
 			}
 			else
-				LIST_APPEND(&udp_proxy_sessions, &sess->list);
+				LIST_APPEND(&shard->buckets[hash & UDP_PROXY_HASH_MASK],
+				            &sess->by_hash);
 		}
 	}
 	if (sess) {
 		sess->srv = srv;
 		sess->expire = tick_add(now_ms, MS_TO_TICKS(UDP_PROXY_SESS_TIMEOUT_MS));
 	}
-	HA_SPIN_UNLOCK(OTHER_LOCK, &udp_proxy_lock);
 
 	return sess;
 }
@@ -237,9 +338,7 @@ void udp_proxy_fd_handler(int fd)
 		if (!sess)
 			continue;
 		if (send(sess->fd, buf->area, ret, 0) < 0) {
-			HA_SPIN_LOCK(OTHER_LOCK, &udp_proxy_lock);
 			udp_proxy_delete_session(sess);
-			HA_SPIN_UNLOCK(OTHER_LOCK, &udp_proxy_lock);
 		}
 	} while (--max_accept);
 }
